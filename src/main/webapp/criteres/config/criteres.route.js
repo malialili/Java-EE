cc4512257ad7b755d05bb02416613706b6ed9edf
malialili <malia.mias@yahoo.fr>
2016-01-29 16:17:22 +0100

    modifffff

--- src/main/webapp/criteres/config/criteres.route.js
+++ src/main/webapp/criteres/config/criteres.route.js
@@ -6,16 +6,16 @@ angular.module('criteres').config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider.
 		when('/criteres', {
-			templateUrl: 'public/personnes/views/list-criteres.client.view.html'
+			templateUrl: 'public/personnes/views/list-criteres.view.html'
 		}).
 		when('/criteres/create', {
-			templateUrl: 'public/personnes/views/create-critere.client.view.html'
+			templateUrl: 'public/personnes/views/create-critere.view.html'
 		}).
 		when('/criteres/:critereId', {
-			templateUrl: 'public/criteres/views/view-critere.client.view.html'
+			templateUrl: 'public/criteres/views/view-critere.view.html'
 		}).
 		when('/criteres/:critereId/edit', {
-			templateUrl: 'public/criteres/views/edit-critere.client.view.html'
+			templateUrl: 'public/criteres/views/edit-critere.view.html'
 		});
 	}
 ]);
